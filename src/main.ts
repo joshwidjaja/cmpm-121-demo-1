74d8a63bb214ed292b6b8c074bcead1a552f9060
Joshua Widjaja <josh@widjaja.us>
2023-10-09 15:27:21 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "cmpm121 D1";
+const clickerEmoji = "🤖";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const clickerButton = document.createElement("button");
+clickerButton.innerHTML = clickerEmoji;
+app.append(clickerButton);
