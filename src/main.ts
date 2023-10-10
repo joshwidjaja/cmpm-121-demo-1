afccfcbfaefcfe9ac8a656d4eaa8ff24373e249b
Joshua Widjaja <josh@widjaja.us>
2023-10-09 22:24:33 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,19 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-app.append(header);
 
 const clickerButton = document.createElement("button");
 clickerButton.innerHTML = clickerEmoji;
+
+let counter: number = 0;
+const tracker = document.createElement("div");
+tracker.innerHTML = `${counter} messages`;
+
+clickerButton.addEventListener('click', () => {
+    counter = counter + 1;
+    tracker.innerHTML = `${counter} messages`;
+});
+
+app.append(header);
 app.append(clickerButton);
+app.append(tracker);
