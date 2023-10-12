cf6a18c5cf9dc74a91a3c779f47b328c8aa0b22f
Joshua Widjaja <josh@widjaja.us>
2023-10-11 19:26:12 -0700

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,10 @@ clickerButton.innerHTML = clickerEmoji;
 app.append(clickerButton);
 
 let counter: number = 0;
-//let firstUpgradeCount = 0;
+let rate: number = 0;
 
 const tracker = document.createElement("div");
-tracker.innerHTML = `${counter} messages`;
+updateCounter();
 app.append(tracker);
 
 clickerButton.addEventListener("click", () => {
@@ -28,34 +28,76 @@ clickerButton.addEventListener("click", () => {
 
 const firstUpgrade = document.createElement("button");
 const firstUpgradeCost: number = 10;
-firstUpgrade.innerHTML = `🍄
+let firstUpgradeCount: number = 0;
+firstUpgrade.innerHTML = `🍄 (${firstUpgradeCount})
 Cost: ${firstUpgradeCost}`;
 firstUpgrade.disabled = true;
 app.append(firstUpgrade);
 
-let rate: number = 0;
+const secondUpgrade = document.createElement("button");
+const secondUpgradeCost: number = 100;
+let secondUpgradeCount: number = 0;
+secondUpgrade.innerHTML = `🐦 (${secondUpgradeCount})
+Cost: ${secondUpgradeCost}`;
+secondUpgrade.disabled = true;
+app.append(secondUpgrade);
+
+const thirdUpgrade = document.createElement("button");
+const thirdUpgradeCost: number = 1000;
+let thirdUpgradeCount: number = 0;
+thirdUpgrade.innerHTML = `🔌 (${thirdUpgradeCount})
+Cost: ${thirdUpgradeCost}`;
+thirdUpgrade.disabled = true;
+app.append(thirdUpgrade);
 
 firstUpgrade.addEventListener("click", () => {
-  //firstUpgradeCount++;
   buyFor(firstUpgradeCost);
-  rate += 1;
+  firstUpgradeCount++;
+  rate += 0.1;
+  firstUpgrade.innerHTML = `🍄 (${firstUpgradeCount})
+Cost: ${firstUpgradeCost}`;
+});
+
+secondUpgrade.addEventListener("click", () => {
+  buyFor(secondUpgradeCost);
+  secondUpgradeCount++;
+  rate += 2;
+  secondUpgrade.innerHTML = `🐦 (${secondUpgradeCount})
+Cost: ${secondUpgradeCost}`;
+});
+
+thirdUpgrade.addEventListener("click", () => {
+  buyFor(thirdUpgradeCost);
+  thirdUpgradeCount++;
+  rate += 50;
+  thirdUpgrade.innerHTML = `🔌 (${thirdUpgradeCount})
+Cost: ${thirdUpgradeCost}`;
 });
 
 window.requestAnimationFrame(clockPlus);
 
+function updateCounter() {
+  tracker.innerHTML = `${counter.toFixed(0)} messages / ${rate.toFixed(
+    1,
+  )} per second`;
+}
+
 function plus(num: number) {
   counter += num;
-  tracker.innerHTML = `${counter} messages`;
+  updateCounter();
 }
 
 function buyFor(cost: number) {
   counter -= cost;
-  tracker.innerHTML = `${counter} messages`;
+  updateCounter();
 }
 
 function clockPlus() {
   const num: number = rate / 60;
   plus(num);
   firstUpgrade.disabled = counter < firstUpgradeCost;
+  secondUpgrade.disabled = counter < secondUpgradeCost;
+  thirdUpgrade.disabled = counter < thirdUpgradeCost;
+
   window.requestAnimationFrame(clockPlus);
 }
